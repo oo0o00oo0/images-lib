f5146194d824c78d911851a8fbddd093781a117f
unknown <matthew@vmistudio.com>
2023-03-25 14:20:41 +0000

    t

--- src/components/SimpleShader/glsl/simpleShaderFrag.glsl
+++ src/components/SimpleShader/glsl/simpleShaderFrag.glsl
@@ -6,9 +6,9 @@ uniform vec2 uPointer;
 
 #define PI 3.141592653589
 
-// float map_linear(float value, float min1, float max1, float min2, float max2) {
-//   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-// }
+float map_linear(float value, float min1, float max1, float min2, float max2) {
+  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
 
 float rectangle(vec2 uv, float size) {
 
@@ -22,7 +22,7 @@ void main() {
 
   vec3 color = vec3(0.0);
 
-  // float mapped = map_linear(uPointer.x, -1., 1., 0., 1.);
+  float mapped = map_linear(uPointer.x, -1., 1., 0., 1.);
 
   color = vec3(rectangle(vUv, .1));
 
